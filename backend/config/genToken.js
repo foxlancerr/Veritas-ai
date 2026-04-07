bbd96c27536c71f0e132aa61a56a0080d3e156ec
Muhammad Asim <muhammadasim.code@gmail.com>
2026-04-08 00:36:03 +0500

    chatbot integration completed

--- backend/config/genToken.js
+++ backend/config/genToken.js
@@ -1,10 +1,20 @@
 import jwt from "jsonwebtoken";
 
 const genToken = (user) => {
-  const token = jwt.sign({user}, process.env.JWT_SECRET, {
+  const token = jwt.sign({ user }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
   return token;
 };
 
-export default genToken;
+const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded.user;
+  } catch (error) {
+    console.error("Token verification error:", error);
+    return null;
+  }
+};
+
+export { genToken, verifyToken };
--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,16 @@ dotenv.config();
 import express from "express";
 import cookieParser from "cookie-parser";
 
-
 import connectDB from "./config/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
 import connectionRouter from "./routes/connection.routes.js";
 import http from "http";
-import { Server } from "socket.io";
 import notificationRouter from "./routes/notification.routes.js";
 import { allowCors } from "./middlewares/allowCors.js";
+import { setupSocket } from "./config/socket.js";
+import chatbotRouter from "./routes/chatbot.routes.js";
 
 const app = express();
 
@@ -22,12 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const PORT = process.env.PORT || 8020;
 const server = http.createServer(app);
-export const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173", // frontend port (Vite default)
-    credentials: true, // allow cookies to be sent
-  },
-});
+export const io = setupSocket(server); // initialize socket with the server
 
 allowCors(app);
 
@@ -36,16 +31,6 @@ app.use("/api/user", userRoutes);
 app.use("/api/connection", connectionRouter);
 app.use("/api/notification", notificationRouter);
 app.use("/api/post", postRoutes);
-
-export const userSocketMap = new Map();
-io.on("connection", (socket) => {
-  socket.on("register", (userId) => {
-    userSocketMap.set(userId, socket.id);
-    console.log(userSocketMap);
-  });
-  socket.on("disconnect", (socket) => {});
-});
-
 server.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
